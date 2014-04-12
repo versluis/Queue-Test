a948b1cafe4928795311e46270019c86005f9e6b
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-04-12 19:05:32 -0400

    Initial Commit

---
+++ QueueTestTests/QueueTestTests.m
@@ -0,0 +1,34 @@
+//
+//  QueueTestTests.m
+//  QueueTestTests
+//
+//  Created by Jay Versluis on 12/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QueueTestTests : XCTestCase
+
+@end
+
+@implementation QueueTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
